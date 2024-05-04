f14f1140a6047791c70bce90e558a65735a0e9ee
HappySunChild <90355743+HappySunChild@users.noreply.github.com>
2024-05-04 00:08:05 -0400

    hello

---
+++ download.lua
@@ -0,0 +1,24 @@
+local function download(link, path)
+	local success = shell.execute('wget', link, path)
+	
+	if not success then
+		download(link, path)
+	end
+end
+
+local baseLink = 'https://raw.githubusercontent.com/HappySunChild/Plethora-Shrug-Client/main/src/%s'
+local files = {
+	'startup.lua',
+	'ShrugModules/bounding.lua',
+	'ShrugModules/button.lua',
+	'ShrugModules/scan.lua',
+	'ShrugModules/fly.lua',
+}
+
+for _, path in ipairs(files) do
+	if fs.exists(path) then
+		fs.delete(path)
+	end
+	
+	download(string.format(baseLink, path), path)
+end
